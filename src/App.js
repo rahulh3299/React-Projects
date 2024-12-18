12afb653b2c8d8d29a3b6fe4aea7f5c05e70efd1
rahul3299 <rahul.hada3299@gmail.com>
2024-12-18 18:11:12 +0530

    add star,plus and minus

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 
-
+import MovieCard from './Moviecard';
 function App() {
   return (
-    <h1>Movie app</h1>
+    <>
+     <h1>Movie app</h1>
+     <MovieCard/>
+    </>
+    
   );
 }
 
---
+++ src/Moviecard.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+const avengers = require("./data/avengers.png");
+export default class MovieCard extends Component{
+   
+    render(){
+        return(
+            <div className="main">
+                <div className="movie-card">
+                    <div className="left">
+                        <img src={avengers} alt="Poster" />
+                    
+                    </div>
+                    
+                    <div className="right">
+                        <div className="title">The Avengers</div>
+                        <div className="plot">Supernatural powers shown in the movie</div>
+                        <div className="price">Rs. 199</div>
+
+                        <div className="footer">
+                            <div className="rating">Rating</div>
+                            <div className="star-dis">
+                                
+                              <img alt="minus" src="https://cdn-icons-png.flaticon.com/128/9146/9146915.png"
+                              className="stars"/>
+                              <img alt="star" src="https://cdn-icons-png.flaticon.com/128/477/477406.png"
+                               className="stars"/>
+                              <img alt="plus" src="https://cdn-icons-png.flaticon.com/128/9312/9312231.png"
+                              className="stars"/> 
+
+                                
+                            </div>
+
+                            <button className="favourite-btn">Favourite</button>
+                            <button className="cart-btn">Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+       
+}
